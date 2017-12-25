262ffe3d97e67a0563af39265cce16c56b475a4b
pitachips_lenovo <hjkim3323@gmail.com>
2017-12-25 19:39:07 +0900

    #10 post_detail 뷰 및 페이지 구현, get_object_or_404 코드 삽입

--- djangofundamentals/blog/views.py
+++ djangofundamentals/blog/views.py
@@ -13,4 +13,22 @@ def post_list(request):
         'q':q,
     })
 
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+
+def post_detail(request, id):
+    # post = Post.objects.get(id=id)  
+    # 이 경우 게시물 미존재 시 http 500 코드를 응답하는 문제
+
+    # try:
+    #     post = Post.objects.get(id=id)
+    # except (Post.DoesNotExist, Post.MultipleObjectsReturned):
+    #     raise Http404
+    # 이렇게 매번 try 구문 쓰는 번거로움을 피하기 위해 get_object_or_404 사용
+
+    post = get_object_or_404(Post, id=id)  # 추천. 게시물 미존재 시 404 코드로 응답
+
+    return render(request, 'blog/post_detail.html', {
+        'post':post,
+    })
 
