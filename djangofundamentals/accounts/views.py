74385264c704cb11d460b9e0be3c3fa6682e40d6
pitachips_lenovo <hjkim3323@gmail.com>
2017-12-28 01:15:41 +0900

    #33 login_required 장식자 부착

--- djangofundamentals/accounts/views.py
+++ djangofundamentals/accounts/views.py
@@ -14,5 +14,12 @@ def signup(request):
     return render(request, 'accounts/signup_form.html', {'form': form})    
 
 
+from django.contrib.auth.decorators import login_required
+
+@login_required
 def profile(request):
     return render(request, 'accounts/profile.html')
+
+# login_required의 역할
+# 1) 비 로그인 상태라면 LOGIN_URL 로 데려감
+# 2) ?next={{ request.path }} 를 자동으로 붙여서 로그인 시 그 url로 갈 수 있게 함
