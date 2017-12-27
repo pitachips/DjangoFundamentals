c0c00260f57b0fd64e6c28bb642ffb5a7715a228
pitachips_lenovo <hjkim3323@gmail.com>
2017-12-27 23:23:08 +0900

    #31 reverse_lazy 이해

--- djangofundamentals/blog/views_cbv.py
+++ djangofundamentals/blog/views_cbv.py
@@ -6,4 +6,15 @@ post_list = ListView.as_view(model=Post, paginate_by=20)
 post_detail = DetailView.as_view(model=Post)
 post_new = CreateView.as_view(model=Post, fields='__all__')
 post_edit = UpdateView.as_view(model=Post, fields='__all__')
-post_delete = DeleteView.as_view(model=Post, success_url='/blog/') # success_url 필수임
+
+
+from django.urls import reverse_lazy
+
+post_delete = DeleteView.as_view(model=Post, success_url=reverse_lazy('blog:post_list')) # success_url 필수임
+
+''' reverse를 사용할 경우 발생하는 에러 메시지
+raise ImproperlyConfigured(msg.format(name=self.urlconf_name))
+django.core.exceptions.ImproperlyConfigured:
+The included URLconf 'djangofundamentals.urls' does not appear to have any patterns in it.
+If you see valid patterns in the file then the issue is probably caused by a circular import.
+'''
