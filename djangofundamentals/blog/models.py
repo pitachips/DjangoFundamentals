feb02cc2ed1b11b2f8fdf1bb0edf16f6bd55c026
pitachips_lenovo <hjkim3323@gmail.com>
2017-12-27 13:16:17 +0900

    #29 방법1 적용) photo 원본 유지, photo_thumbnail 별도 필드 생성

--- djangofundamentals/blog/models.py
+++ djangofundamentals/blog/models.py
@@ -11,6 +11,8 @@ def lnglat_validator(value):
     if not re.match(r'^([+-]?\d+.?\d*),\s?([+-]?\d+.?\d*)$', value):  #\s?는 띄어쓰기가 0 또는 1개 있음을 의미
         raise ValidationError('Invalid lnglat')
 
+from imagekit.models import ImageSpecField
+from imagekit.processors import Thumbnail
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -22,6 +24,12 @@ class Post(models.Model):
     title = models.CharField(max_length=100, verbose_name='제목', help_text='100자 이내로 제목을 적어주세요')  # 길이제한이 있는 문자열
     content = models.TextField(verbose_name="내용")   # 길이제한이 없는 문자열. DB를 위한 구분
     photo = models.ImageField(blank=True, upload_to="blog/post/%Y/%m/%d") # 앞뒤로 슬래시 없어야함! 
+    photo_thumbnail = ImageSpecField(
+        source='photo',
+        processors=[Thumbnail(300,300)],
+        format='jpeg',
+        options={'quality':70}    
+    )
     tags = models.CharField(max_length=100, blank=True)
     tag_set = models.ManyToManyField('Tag', blank=True)   # Tag를 문자열로 감싸서 넣어줄 것. 그래야 undefined 에러 피할 수 있음
     status = models.CharField(max_length=1, choices=STATUS_CHOICES)
