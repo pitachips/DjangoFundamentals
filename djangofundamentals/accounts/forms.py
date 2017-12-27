cedd66a06794fa9e7d12b40486368ac9210cee3b
pitachips_lenovo <hjkim3323@gmail.com>
2017-12-28 02:32:47 +0900

    #34 AuthenticationForm 상속받은 LoginForm 퀴즈 구현

--- djangofundamentals/accounts/forms.py
+++ djangofundamentals/accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import Profile
 
 class SignupForm(UserCreationForm):
@@ -12,10 +12,18 @@ class SignupForm(UserCreationForm):
         # email은 class AbstractUser에서 email 필드가 있기때문에 fields에 넣는것만으로 곧장 사용가능
 
     def save(self):
-        user = super().save()   # UserCreationForm은 model로 User를 두고있으므로 email 값도 여기서 저장됨
+        user = super().save()  # UserCreationForm은 model로 User를 두고있으므로 email 값도 여기서 저장됨. 뷰에서 이미 email은 받아져있는 상태임.
         profile = Profile.objects.create(
             user=user,
             phone_number = self.cleaned_data['phone_number'],
             address = self.cleaned_data['address']
         )
         return user
+
+
+class LoginForm(AuthenticationForm):
+    answer = forms.IntegerField(label='3+3=?')
+
+    def clean_answer(self):
+        if self.cleaned_data.get('answer', None) != 6:
+            raise forms.ValidationError('땡~!')
