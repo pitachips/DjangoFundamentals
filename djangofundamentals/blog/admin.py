fac56d8ee22c434a7642f4616f3f9bd48f4a8e1c
pitachips_lenovo <hjkim3323@gmail.com>
2017-12-28 14:55:46 +0900

    #35 admin에 select_related와 prefetch_related 적용

--- djangofundamentals/blog/admin.py
+++ djangofundamentals/blog/admin.py
@@ -19,8 +19,12 @@ from django.utils.html import mark_safe
 # 기본 admin + 커스터마이징 (2. 장식자) 사용 추천
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display=['id', 'title', 'status', 'content_size', 'created_at', 'updated_at']
+    list_display=['id', 'title', 'status', 'content_size', 'tag_list', 'created_at', 'updated_at']
     actions = ['make_published', 'make_draft']
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.prefetch_related('tag_set')
 
     # 포스트 내용의 글자 수를 카운트 하는 함수를 만들어서 보여줄 수 있음
     # 인자로는 1)self, 2)Post 모델의 인스턴스
@@ -28,6 +32,10 @@ class PostAdmin(admin.ModelAdmin):
         return mark_safe('<strong>{}</strong>글자'.format(len(post.content)))
     # list_display에는 소괄호 없이 함수의 이름만을 넣는다
     content_size.short_description = '글자수'
+
+    # get_queryset 재정의 없으면 이 함수는 각 post에 대응하는 tag를 가져오기 위해 post갯수만큼 DB에 접근
+    def tag_list(self, post):
+        return ', '.join(tag.name for tag in post.tag_set.all())
 
     # bulk action을 만들기 위한 함수는 항상 인자로 self, request, queryset을 받음 
     def make_published(self, request, queryset):
@@ -42,8 +50,26 @@ class PostAdmin(admin.ModelAdmin):
 
 
 @admin.register(Comment)
-class PostAdmin(admin.ModelAdmin):
-    pass
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'message', 'post_status', 'post_title', 'post_content_len']
+
+    def post_status(self, comment):
+        return comment.post.status
+
+    def post_title(self, comment):
+        return comment.post.title
+
+    def post_content_len(self, comment):  # 여기는 Comment 모델이 아니라 CommentAdmin 클래스이므로 comment 별도로 인자로 넣어줘야 함
+        return len(comment.post.content)
+
+    # list_select_related = ['post'] 
+    # select_related 관계에 대해서는 간편한 인터페이스 제공되나 아래와 같이 코딩할 수도 있음
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.select_related('post')
+
+
+
 
 
 @admin.register(Tag)
